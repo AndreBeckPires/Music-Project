a6c36ef59bc3d37ad868ba254254b82fcf70713d
fabriciovo <fabricio.v.o@hotmail.com>
2022-09-25 21:17:59 -0300

    Add music

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+    public AudioSource hitSFX;
+    public AudioSource missSFX;
+    public TMPro.TextMeshPro scoreText;
+    static int comboScore;
+    void Start()
+    {
+        Instance = this;
+        comboScore = 0;
+    }
+    public static void Hit()
+    {
+        //When hit player make the hit sound and decrease player life
+        //Instance.hitSFX.Play();
+    }
+    public static void Point()
+    {
+        comboScore++;
+
+    }
+    private void Update()
+    {
+        scoreText.text = comboScore.ToString();
+    }
+}
