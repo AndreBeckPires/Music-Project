69be884e650ca63ce4eaee1a2d75b0ecfb935ef4
André Pires <andrebeckpires@gmail.com>
2022-09-29 18:11:25 -0300

    tileset movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Tilemap groundTileMap;
+    [SerializeField]
+    private Tilemap collisionTileMap;
+    private PlayerMovement controls;
+
+
+    private void Awake(){
+        controls = new PlayerMovement();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Start()
+    {
+        controls.Main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void Move(Vector2 direction){
+        if(CanMove(direction))
+        {
+            transform.position += (Vector3)direction;
+        }
+    }
+
+    private bool CanMove(Vector2 direction){
+        Vector3Int gridPosition = groundTileMap.WorldToCell(transform.position +(Vector3)direction);
+        if(!groundTileMap.HasTile(gridPosition) || collisionTileMap.HasTile(gridPosition))
+            return false;
+        return true;
+    }
+    // Update is called once per frame
+  
+}
